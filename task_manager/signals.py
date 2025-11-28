5b379e7a8a3e720e123af7be4fee044335cc0ea4
Георгий Малков <georgijmalkov@MacBook-Air-Georgij.local>
2025-11-28 17:48:23 +0400

    fix

--- task_manager/signals.py
+++ task_manager/signals.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.signals import user_logged_in
+from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
 from django.contrib import messages
 
@@ -6,4 +6,9 @@ from django.contrib import messages
 @receiver(user_logged_in)
 def on_user_login(sender, request, user, **kwargs):
     messages.success(request, 'Вы залогинены', fail_silently=True)
+
+
+@receiver(user_logged_out)
+def on_user_logout(sender, request, user, **kwargs):
+    messages.success(request, 'Вы разлогинены', fail_silently=True)
 
