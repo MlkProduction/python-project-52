b10bae2d797062b20660124f9e955d96793e3ab2
Георгий Малков <georgijmalkov@MacBook-Air-Georgij.local>
2025-11-28 19:02:37 +0400

    флеш сообщение

--- task_manager/views.py
+++ task_manager/views.py
@@ -41,6 +41,7 @@ def users_create(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Пользователь успешно зарегистрирован')
             return redirect("login")
     else:
         form = RegistrationForm()
