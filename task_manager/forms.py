81ac04a0468ab967ee453e0a2ce7b2e3701647ee
Георгий Малков <georgijmalkov@MacBook-Air-Georgij.local>
2025-11-30 17:06:14 +0400

    fix

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -34,10 +34,39 @@ class UserUpdateForm(forms.ModelForm):
     first_name = forms.CharField(max_length=30, required=True, label="Имя")
     last_name = forms.CharField(max_length=30, required=True, label="Фамилия")
     username = forms.CharField(max_length=150, required=True, label="Имя пользователя")
+    password1 = forms.CharField(
+        widget=forms.PasswordInput,
+        required=False,
+        label="Пароль"
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput,
+        required=False,
+        label="Подтверждение пароля"
+    )
     
     class Meta:
         model = User
         fields = ("username", "first_name", "last_name")
+    
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2:
+            if password1 != password2:
+                raise forms.ValidationError("Пароли не совпадают")
+        return password2
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        password = self.cleaned_data.get('password1')
+        if password:
+            user.set_password(password)
+        if commit:
+            user.save()
+        return user
 
 class StatusesCreateForm(forms.ModelForm):
     class Meta:
